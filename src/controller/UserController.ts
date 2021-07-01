d49c477e46424ae7ac0c25cac5606564cf5cad38
Thiago Novaes <novaesthago@gmail.com>
2021-07-01 19:08:10 -0300

    Frist commit

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -6,10 +6,6 @@ import * as bcrypt from "bcryptjs";
 class UserController {
     async store(Request: Request, Response: Response) {
 
-        /**
-         * Validação através do YUP schema
-         * Início
-         */
         let schema = yup.object().shape({
             last_name: yup.string().required(),
             frist_name: yup.string().required(),
@@ -24,16 +20,6 @@ class UserController {
           })
         }
     
-         /**
-         * Validação através do YUP schema
-         * fim
-         */
-    
-        /**
-         * Validação no banco de dados
-         * Verifica se o usuário existe
-         */
-    
         let userExist = await UserModel.findOne({ email: Request.body.email });
         if(userExist) {
           return Response.status(400).json({
@@ -41,28 +27,11 @@ class UserController {
             message: "Este usuário já existe!"
           })
         }
-    
-         /**
-          * Desestrutuação dos dados da requisição
-          */
-        const { frist_name, last_name, email, password } = Request.body;
-    
-        /**
-          * criação da constante data
-          */
         
-    
+        const { frist_name, last_name, email, password } = Request.body;      
         const data = { frist_name, last_name, email, password };
     
-        /**
-          * Criptografar a senha
-          */
-    
         data.password = await bcrypt.hash(data.password, 8);
-    
-        /**
-          * Inserção no banco de dados 
-          */
     
         UserModel.create(data, (err) => {
           if(err) return Response.status(400).json({
